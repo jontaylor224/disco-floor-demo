14613cf3496c00089df5fa86ac34cf863626e718
Jon Taylor <jontaylor224@gmail.com>
2022-05-03 08:35:21 -0500

    added start button, hover CSS

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 
 
 const backgroundColors = ["darkorange","pink", "cyan", "yellow", "white", "dodgerblue", "fuchsia", "greenyellow", "skyblue", "deeppink" ]
-
-
 const cells = document.querySelectorAll(".cell")
-
-console.log(cells)
+const startButton = document.querySelector("#start")
+const stopButton = document.querySelector("#stop")
+let colorTimer
 
 function changeColors () {
     for (let index = 0; index < cells.length; index += 1) {
@@ -14,10 +13,16 @@ function changeColors () {
     }
 }
 
-// changeColors()
+function animateFloor() {
+    if (!colorTimer){
+        colorTimer = setInterval(changeColors, 3000)
+    }
+}
 
-let colorTimer = setInterval(changeColors, 3000)
+stopButton.addEventListener("click", () => {
+    clearInterval(colorTimer)
+    colorTimer = null
+    }
+)
 
-let stopButton = document.querySelector("#stop")
-
-stopButton.addEventListener("click", () => clearInterval(colorTimer))
+startButton.addEventListener("click", animateFloor)
