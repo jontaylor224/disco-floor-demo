b204954899221dbc3baa9158839fc53ca101b966
Jon Taylor <jontaylor224@gmail.com>
2022-05-06 16:35:01 -0500

    replaced hardcoded grid with function

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 
 
 const backgroundColors = ["darkorange","pink", "cyan", "yellow", "white", "dodgerblue", "fuchsia", "greenyellow", "skyblue", "deeppink" ]
-const cells = document.querySelectorAll(".cell")
+const gridElement = document.querySelector("#grid")
 const startButton = document.querySelector("#start")
 const stopButton = document.querySelector("#stop")
 let colorTimer
+
+function createGrid (height = 7, width = 7) {
+    for (let i = 0; i < height; i += 1) {
+        let rowElement = document.createElement("div")
+        rowElement.classList.add("row")
+        for (let j = 0; j < width; j += 1) {
+            let cellElement = document.createElement("div")
+            cellElement.classList.add("cell")
+            rowElement.append(cellElement)
+        }
+        gridElement.append(rowElement)
+    }
+}
+createGrid()
+const cells = document.querySelectorAll(".cell")
 
 function changeColors () {
     for (let index = 0; index < cells.length; index += 1) {
